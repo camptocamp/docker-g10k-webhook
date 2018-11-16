cd4629af8a47e139d8458018aa2643e158b0225e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-11-16 14:25:37 +0100

    Initialize if r10k.yaml is present only

--- docker-entrypoint.d/05-initialize-code.sh
+++ docker-entrypoint.d/05-initialize-code.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-touch /etc/puppetlabs/code/environments/r10k-initializing.lock
-/nss_wrapper.sh g10k -config /etc/puppetlabs/r10k/r10k.yaml -info
-rm /etc/puppetlabs/code/environments/r10k-initializing.lock
+
+if test -f /etc/puppetlabs/r10k/r10k.yaml; then
+  touch /etc/puppetlabs/code/environments/r10k-initializing.lock
+  /nss_wrapper.sh g10k -config /etc/puppetlabs/r10k/r10k.yaml -info
+  rm /etc/puppetlabs/code/environments/r10k-initializing.lock
+fi

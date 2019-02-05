e22ba847204e29b5335d17003303091b63e40db3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-02-05 17:53:24 +0100

    upper case exported variables

--- nss_wrapper.sh
+++ nss_wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if ! getent passwd "$(id -u)" &> /dev/null && [ -e /usr/lib/libnss_wrapper.so ]; then
-	export ld_preload='/usr/lib/libnss_wrapper.so'
-	export nss_wrapper_passwd="$(mktemp)"
-	export nss_wrapper_group="$(mktemp)"
+	export LD_PRELOAD='/usr/lib/libnss_wrapper.so'
+	export NSS_WRAPPER_PASSWD="$(mktemp)"
+	export NSS_WRAPPER_GROUP="$(mktemp)"
 	echo "webhook:x:$(id -u):$(id -g):webhook:/:/bin/false" > "$nss_wrapper_passwd"
 	echo "webhook:x:$(id -g):" > "$nss_wrapper_group"
 fi

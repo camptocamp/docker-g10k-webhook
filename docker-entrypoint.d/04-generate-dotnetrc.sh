986c49dff4bcb41235b6c49fd5ca3e71db9f640a
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-07 16:21:33 +0200

    Allow to pass GITHUB_USER

--- docker-entrypoint.d/04-generate-dotnetrc.sh
+++ docker-entrypoint.d/04-generate-dotnetrc.sh
@@ -4,7 +4,7 @@ set -e
 if test -n "${GITHUB_TOKEN}"; then
 
 # Retrieve GITHUB_USER from GITHUB_TOKEN
-GITHUB_USER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/user | sed -n '/ *"login": "\(.*\)",$/ s//\1/p')
+: GITHUB_USER=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/user | sed -n '/ *"login": "\(.*\)",$/ s//\1/p')
 
 if test -n "${GITHUB_USER}" && test -n "${GITHUB_TOKEN}"; then
   cat << EOF > ~/.netrc
